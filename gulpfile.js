4e731d49c28cd1a03f9857dcd3b00ff546c2c7e8
Eason Jiang <EasonJiangabc@gmail.com>
2022-03-10 22:41:57 -0600

    removed stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,1 @@
-'use strict';var gulp = require('gulp');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');sass.compiler = require('node-sass');gulp.task('sass', function () {   return gulp.src('./sass/**/*.scss')
-   .pipe(concat('custom.scss'))
-   .pipe(sass().on('error', sass.logError))
-   .pipe(gulp.dest('./dist/'));});
-
 exports.build=series(scssTask,jsTask);
