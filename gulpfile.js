be013be79bfbebb497cc09bd572ff7a0f328d0ae
Eason Jiang <EasonJiangabc@gmail.com>
2022-03-11 07:07:54 -0600

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ const browsersync = require('browser-sync').create();
 
 // Sass Task
 function scssTask() {
-  return src('C:/This Pc/documents/GitHub/crypto_website/main.scss', { sourcemaps: true })
+  return src('main.scss', { sourcemaps: true })
     .pipe(sass())
     .pipe(postcss([autoprefixer(), cssnano()]))
     .pipe(dest('dist', { sourcemaps: '.' }));
@@ -20,43 +20,12 @@ function scssTask() {
 
 // JavaScript Task
 function jsTask() {
-  return src('GitHub/crypto_website/index.js', { sourcemaps: true })
+  return src('main.js', { sourcemaps: true })
     .pipe(babel({ presets: ['@babel/preset-env'] }))
     .pipe(terser())
     .pipe(dest('dist', { sourcemaps: '.' }));
 }
 
-// Browsersync
-function browserSyncServe(cb) {
-  browsersync.init({
-    server: {
-      baseDir: '.',
-    },
-    notify: {
-      styles: {
-        top: 'auto',
-        bottom: '0',
-      },
-    },
-  });
-  cb();
-}
-function browserSyncReload(cb) {
-  browsersync.reload();
-  cb();
-}
-
-// Watch Task
-function watchTask() {
-  watch('*.html', browserSyncReload);
-  watch(
-    ['GitHub/crypto_website/**/*.scss', 'crypto_website/**/*.js'],
-    series(scssTask, jsTask, browserSyncReload)
-  );
-}
-
-// Default Gulp Task
-exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
 
 // Build Gulp Task
 exports.build = series(scssTask, jsTask);
