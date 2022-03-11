544995bebd9d710452eba84d39588686ed154628
Eason Jiang <EasonJiangabc@gmail.com>
2022-03-10 22:54:59 -0600

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
+const{src,dest,watch,series} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-import dartSass from 'sass';
-import gulpSass from 'gulp-sass';
-const sass = gulpSass(dartSass);
+const postcss = require('gulp-postcss');
+const autoprefixer=require('autoprefixer');
+const cssnano=require('cssnano');
+const babel = require('gulp-babel')
+const terser=require('gulp-terser');
+const browsersync-require('browser-sync').create();
+
 exports.build=series(scssTask,jsTask);
