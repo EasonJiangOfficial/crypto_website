95bacc7da75328c24b2bf9b6cb73adc1999aec28
Eason Jiang <EasonJiangabc@gmail.com>
2022-03-10 22:47:33 -0600

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,5 @@
+const sass = require('gulp-sass')(require('sass'));
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+const sass = gulpSass(dartSass);
 exports.build=series(scssTask,jsTask);
